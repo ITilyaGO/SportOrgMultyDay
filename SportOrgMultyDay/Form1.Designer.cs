a2528e177adfb089e1747ab28318316bcceab16d
ilya_GO <42610063+ITilyaGO@users.noreply.github.com>
2022-06-24 01:11:25 +0300

    v0.3 Multy day with SportOrg file

--- SportOrgMultyDay/Form1.Designer.cs
+++ SportOrgMultyDay/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.buttonImportHtml = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialogHtml = new System.Windows.Forms.OpenFileDialog();
             this.richTextBoxOut = new System.Windows.Forms.RichTextBox();
             this.labelRaceIndex = new System.Windows.Forms.Label();
             this.buttonCombine = new System.Windows.Forms.Button();
@@ -42,11 +42,24 @@
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.labelDay1Info = new System.Windows.Forms.Label();
             this.labelDay2Info = new System.Windows.Forms.Label();
+            this.buttonImportRaceJson = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.labelBibFinded = new System.Windows.Forms.Label();
+            this.buttonImportBib = new System.Windows.Forms.Button();
+            this.buttonProcessing = new System.Windows.Forms.Button();
+            this.labelRaceFindedDays = new System.Windows.Forms.Label();
+            this.buttonExportBibs = new System.Windows.Forms.Button();
+            this.openFileDialogJson = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonImportHtml
             // 
-            this.buttonImportHtml.Location = new System.Drawing.Point(167, 5);
+            this.buttonImportHtml.Location = new System.Drawing.Point(161, 8);
             this.buttonImportHtml.Name = "buttonImportHtml";
             this.buttonImportHtml.Size = new System.Drawing.Size(97, 23);
             this.buttonImportHtml.TabIndex = 0;
@@ -54,14 +67,14 @@
             this.buttonImportHtml.UseVisualStyleBackColor = true;
             this.buttonImportHtml.Click += new System.EventHandler(this.buttonImportHtml_Click);
             // 
-            // openFileDialog1
+            // openFileDialogHtml
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            this.openFileDialog1.Filter = "Html|*.html|All files|*.*";
+            this.openFileDialogHtml.FileName = "openFileDialog1";
+            this.openFileDialogHtml.Filter = "Html|*.html|All files|*.*";
             // 
             // richTextBoxOut
             // 
-            this.richTextBoxOut.Location = new System.Drawing.Point(474, 13);
+            this.richTextBoxOut.Location = new System.Drawing.Point(474, 12);
             this.richTextBoxOut.Name = "richTextBoxOut";
             this.richTextBoxOut.Size = new System.Drawing.Size(314, 292);
             this.richTextBoxOut.TabIndex = 1;
@@ -78,7 +91,7 @@
             // 
             // buttonCombine
             // 
-            this.buttonCombine.Location = new System.Drawing.Point(167, 63);
+            this.buttonCombine.Location = new System.Drawing.Point(161, 66);
             this.buttonCombine.Name = "buttonCombine";
             this.buttonCombine.Size = new System.Drawing.Size(137, 23);
             this.buttonCombine.TabIndex = 4;
@@ -88,7 +101,7 @@
             // 
             // buttonImportHtml2
             // 
-            this.buttonImportHtml2.Location = new System.Drawing.Point(167, 34);
+            this.buttonImportHtml2.Location = new System.Drawing.Point(161, 37);
             this.buttonImportHtml2.Name = "buttonImportHtml2";
             this.buttonImportHtml2.Size = new System.Drawing.Size(97, 23);
             this.buttonImportHtml2.TabIndex = 5;
@@ -98,7 +111,7 @@
             // 
             // textBoxStart1
             // 
-            this.textBoxStart1.Location = new System.Drawing.Point(61, 5);
+            this.textBoxStart1.Location = new System.Drawing.Point(55, 8);
             this.textBoxStart1.Name = "textBoxStart1";
             this.textBoxStart1.PlaceholderText = "Старт 01.01";
             this.textBoxStart1.Size = new System.Drawing.Size(100, 23);
@@ -106,7 +119,7 @@
             // 
             // textBoxStart2
             // 
-            this.textBoxStart2.Location = new System.Drawing.Point(61, 35);
+            this.textBoxStart2.Location = new System.Drawing.Point(55, 38);
             this.textBoxStart2.Name = "textBoxStart2";
             this.textBoxStart2.PlaceholderText = "Старт 02.01";
             this.textBoxStart2.Size = new System.Drawing.Size(100, 23);
@@ -115,7 +128,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(6, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(43, 15);
             this.label1.TabIndex = 8;
@@ -124,7 +137,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Location = new System.Drawing.Point(6, 41);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(43, 15);
             this.label2.TabIndex = 8;
@@ -132,7 +145,7 @@
             // 
             // buttonPutToBib
             // 
-            this.buttonPutToBib.Location = new System.Drawing.Point(167, 92);
+            this.buttonPutToBib.Location = new System.Drawing.Point(161, 95);
             this.buttonPutToBib.Name = "buttonPutToBib";
             this.buttonPutToBib.Size = new System.Drawing.Size(115, 23);
             this.buttonPutToBib.TabIndex = 9;
@@ -147,7 +160,7 @@
             // labelDay1Info
             // 
             this.labelDay1Info.AutoSize = true;
-            this.labelDay1Info.Location = new System.Drawing.Point(266, 9);
+            this.labelDay1Info.Location = new System.Drawing.Point(260, 12);
             this.labelDay1Info.Name = "labelDay1Info";
             this.labelDay1Info.Size = new System.Drawing.Size(0, 15);
             this.labelDay1Info.TabIndex = 10;
@@ -155,30 +168,135 @@
             // labelDay2Info
             // 
             this.labelDay2Info.AutoSize = true;
-            this.labelDay2Info.Location = new System.Drawing.Point(266, 38);
+            this.labelDay2Info.Location = new System.Drawing.Point(260, 41);
             this.labelDay2Info.Name = "labelDay2Info";
             this.labelDay2Info.Size = new System.Drawing.Size(0, 15);
             this.labelDay2Info.TabIndex = 11;
+            // 
+            // buttonImportRaceJson
+            // 
+            this.buttonImportRaceJson.Location = new System.Drawing.Point(6, 6);
+            this.buttonImportRaceJson.Name = "buttonImportRaceJson";
+            this.buttonImportRaceJson.Size = new System.Drawing.Size(179, 23);
+            this.buttonImportRaceJson.TabIndex = 12;
+            this.buttonImportRaceJson.Text = "Выбрать файл соревнований";
+            this.buttonImportRaceJson.UseVisualStyleBackColor = true;
+            this.buttonImportRaceJson.Click += new System.EventHandler(this.buttonImportRaceJson_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(456, 292);
+            this.tabControl1.TabIndex = 13;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.buttonImportHtml);
+            this.tabPage1.Controls.Add(this.labelDay2Info);
+            this.tabPage1.Controls.Add(this.buttonCombine);
+            this.tabPage1.Controls.Add(this.labelDay1Info);
+            this.tabPage1.Controls.Add(this.buttonImportHtml2);
+            this.tabPage1.Controls.Add(this.buttonPutToBib);
+            this.tabPage1.Controls.Add(this.textBoxStart1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.textBoxStart2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(448, 264);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Комбинирование";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.labelBibFinded);
+            this.tabPage2.Controls.Add(this.buttonImportBib);
+            this.tabPage2.Controls.Add(this.buttonProcessing);
+            this.tabPage2.Controls.Add(this.labelRaceFindedDays);
+            this.tabPage2.Controls.Add(this.buttonExportBibs);
+            this.tabPage2.Controls.Add(this.buttonImportRaceJson);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(448, 244);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Файла соревнований";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // labelBibFinded
+            // 
+            this.labelBibFinded.AutoSize = true;
+            this.labelBibFinded.Location = new System.Drawing.Point(191, 39);
+            this.labelBibFinded.Name = "labelBibFinded";
+            this.labelBibFinded.Size = new System.Drawing.Size(36, 15);
+            this.labelBibFinded.TabIndex = 20;
+            this.labelBibFinded.Text = "None";
+            // 
+            // buttonImportBib
+            // 
+            this.buttonImportBib.Location = new System.Drawing.Point(6, 35);
+            this.buttonImportBib.Name = "buttonImportBib";
+            this.buttonImportBib.Size = new System.Drawing.Size(179, 23);
+            this.buttonImportBib.TabIndex = 19;
+            this.buttonImportBib.Text = "Выбрать файл номеров";
+            this.buttonImportBib.UseVisualStyleBackColor = true;
+            this.buttonImportBib.Click += new System.EventHandler(this.buttonImportBib_Click);
+            // 
+            // buttonProcessing
+            // 
+            this.buttonProcessing.Location = new System.Drawing.Point(6, 78);
+            this.buttonProcessing.Name = "buttonProcessing";
+            this.buttonProcessing.Size = new System.Drawing.Size(179, 23);
+            this.buttonProcessing.TabIndex = 17;
+            this.buttonProcessing.Text = "Обработать";
+            this.buttonProcessing.UseVisualStyleBackColor = true;
+            this.buttonProcessing.Click += new System.EventHandler(this.buttonProcessing_Click);
+            // 
+            // labelRaceFindedDays
+            // 
+            this.labelRaceFindedDays.AutoSize = true;
+            this.labelRaceFindedDays.Location = new System.Drawing.Point(191, 10);
+            this.labelRaceFindedDays.Name = "labelRaceFindedDays";
+            this.labelRaceFindedDays.Size = new System.Drawing.Size(36, 15);
+            this.labelRaceFindedDays.TabIndex = 16;
+            this.labelRaceFindedDays.Text = "None";
+            // 
+            // buttonExportBibs
+            // 
+            this.buttonExportBibs.Location = new System.Drawing.Point(6, 123);
+            this.buttonExportBibs.Name = "buttonExportBibs";
+            this.buttonExportBibs.Size = new System.Drawing.Size(179, 23);
+            this.buttonExportBibs.TabIndex = 15;
+            this.buttonExportBibs.Text = "Экспорт номеров";
+            this.buttonExportBibs.UseVisualStyleBackColor = true;
+            this.buttonExportBibs.Click += new System.EventHandler(this.buttonExportBibs_Click);
+            // 
+            // openFileDialogJson
+            // 
+            this.openFileDialogJson.FileName = "openFileDialog1";
+            this.openFileDialogJson.Filter = "Json|*.json|All files|*.*";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.labelDay2Info);
-            this.Controls.Add(this.labelDay1Info);
-            this.Controls.Add(this.buttonPutToBib);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBoxStart2);
-            this.Controls.Add(this.textBoxStart1);
-            this.Controls.Add(this.buttonImportHtml2);
-            this.Controls.Add(this.buttonCombine);
+            this.ClientSize = new System.Drawing.Size(800, 328);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.labelRaceIndex);
             this.Controls.Add(this.richTextBoxOut);
-            this.Controls.Add(this.buttonImportHtml);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -187,7 +305,7 @@
         #endregion
 
         private Button buttonImportHtml;
-        private OpenFileDialog openFileDialog1;
+        private OpenFileDialog openFileDialogHtml;
         private RichTextBox richTextBoxOut;
         private Label labelRaceIndex;
         private Button buttonCombine;
@@ -200,5 +318,15 @@
         private SaveFileDialog saveFileDialog1;
         private Label labelDay1Info;
         private Label labelDay2Info;
+        private Button buttonImportRaceJson;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Label labelRaceFindedDays;
+        private Button buttonExportBibs;
+        private OpenFileDialog openFileDialogJson;
+        private Button buttonProcessing;
+        private Button buttonImportBib;
+        private Label labelBibFinded;
     }
 }
--- SportOrgMultyDay/Form1.cs
+++ SportOrgMultyDay/Form1.cs
@@ -8,6 +8,10 @@ namespace SportOrgMultyDay
        // string Html;
         string jsonSource1;
         string jsonSource2;
+        JObject jsonRace;
+        string htmlPattern = "null";
+        string htmlBib = "null";
+        JObject jsonBib;
 
         public Form1()
         {
@@ -16,7 +20,9 @@ namespace SportOrgMultyDay
 
         private void buttonImportHtml_Click(object sender, EventArgs e)
         {
-            jsonSource1 = FindJson(ImportHtml());
+            string inHtml = ImportHtml();
+            if (inHtml == "null") return;
+            jsonSource1 = FindJson(inHtml);
             var data1 = JObject.Parse(jsonSource1);
             //var data1pers = data1["persons"];
             labelDay1Info.Text = "Участники:" + data1["persons"].Count();
@@ -31,6 +37,23 @@ namespace SportOrgMultyDay
             shablon = shablon.Replace("<|JsonRace|>", json);
             shablon = shablon.Replace("<|StartTime|>", textBoxStart1.Text);
             shablon = shablon.Replace("<|StartTime2|>", textBoxStart2.Text);
+            File.WriteAllText(saveFileDialog1.FileName, shablon);
+        }
+
+        private void ExportNumbers(string json)
+        {
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK) return;
+            string shablon = htmlPattern;
+            if (shablon == "null")
+            {
+
+                shablon = htmlBib;
+                shablon = shablon.Replace(FindJson(htmlBib), json);
+            }
+            else
+                shablon = shablon.Replace("<|JsonRace|>", json);
+
+
             File.WriteAllText(saveFileDialog1.FileName, shablon);
         }
 
@@ -64,6 +87,69 @@ namespace SportOrgMultyDay
             return newjson;
         }
 
+        private string ProcessJson(JObject jbib, JObject joRace)
+        {
+            var races = joRace["races"];
+            var bibPersons = jbib["persons"];
+            var jOut = jbib.DeepClone();
+            JArray jOutPersons = (JArray)jOut["persons"];
+
+            int racesCount = races.Count();
+            for (int i = 0; i < jOutPersons.Count(); i++)
+            {
+                jOutPersons[i]["start_times"] = CreateStartTimes(racesCount);
+            }
+
+            for (int rc = 0; rc < races.Count(); rc++)
+            {
+                var curRace = races[rc];
+                var curPersons = curRace["persons"];
+
+                for (int cp = 0; cp < curPersons.Count(); cp++)
+                {
+                    bool searched = false;
+                    var curPers = curPersons[cp];
+                    int curBib = (int)curPers["bib"];
+                    JToken joPers = null;
+                    for (int jop = 0; jop < jOutPersons.Count(); jop++)
+                    {
+                        int bpBib = (int)jOutPersons[jop]["bib"];
+                        if (bpBib == curBib)
+                        {
+                            joPers = jOutPersons[jop];
+                            searched = true;
+                            break;
+                        }
+                    }
+                    if (searched)
+                    {
+                        JArray joStartTimes = (JArray)joPers["start_times"];
+                        joStartTimes[rc] = curPers["start_time"];
+                    }
+                    else
+                    {
+                        JToken persToInsert = curPers.DeepClone();
+                        persToInsert["start_times"] = CreateStartTimes(racesCount);
+                        persToInsert["start_times"][rc] = curPers["start_time"];
+                        jOutPersons.Add(persToInsert);
+                    }
+                }
+            }
+
+
+            string newjson = jOut.ToString(Newtonsoft.Json.Formatting.None);
+            return newjson;
+        }
+        private JArray CreateStartTimes(int count)
+        {
+            JArray st = new JArray();
+            for (int i = 0; i < count; i++)
+            {
+                st.Add(0);
+            }
+            return st;
+        }
+
         private string FindJson(string html)
         {
             int jsonStart = html.IndexOf(StartStrJson) + StartStrJson.Length;
@@ -77,10 +163,38 @@ namespace SportOrgMultyDay
 
         private string ImportHtml()
         {
-            openFileDialog1.ShowDialog();
-            string Html = File.ReadAllText(openFileDialog1.FileName);
+            if (openFileDialogHtml.ShowDialog() != DialogResult.OK) return "null";
+            string Html = File.ReadAllText(openFileDialogHtml.FileName);
             return Html;
            // jsonSource2 = FindJson(Html);
+        }
+
+        private JObject ImportJson()
+        {
+            openFileDialogJson.ShowDialog();
+            string json = File.ReadAllText(openFileDialogJson.FileName);
+            JObject jobj = JObject.Parse(json);
+            return jobj;
+        }
+
+        private int GetRaceDayCount(JObject jobo)
+        {
+            try
+            {
+                var race = jobo;
+                int days = race["races"].Count();
+                return days;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Парсинг файла не удался.","Возможно файл соревнований однодневный, попробуйте добавить еще день.\n\n" + ex.Message);
+            }
+            return 0;
+        }
+        private bool GetPatternDayCount(string pattrn)
+        {
+            return pattrn.Contains("<|JsonRace|>");
         }
 
         private void buttonImportHtml2_Click(object sender, EventArgs e)
@@ -88,7 +202,6 @@ namespace SportOrgMultyDay
             jsonSource2 = FindJson(ImportHtml());
             var data2 = JObject.Parse(jsonSource1);
             labelDay2Info.Text = "Участники:" + data2["persons"].Count();
-
             // richTextBoxOut.Text = jsonSource2;
         }
 
@@ -102,6 +215,38 @@ namespace SportOrgMultyDay
             //  openFileDialog1.ShowDialog();
             //  string sh = File.ReadAllText(openFileDialog1.FileName);
             PutToNumbers(richTextBoxOut.Text);
+        }
+
+        private void buttonImportRaceJson_Click(object sender, EventArgs e)
+        {
+            jsonRace = ImportJson();
+            labelRaceFindedDays.Text = "Найдено дней: " + GetRaceDayCount(jsonRace);
+        }
+
+        private void buttonProcessing_Click(object sender, EventArgs e)
+        {
+            richTextBoxOut.Text = ProcessJson(jsonBib, jsonRace);
+        }
+
+        private void buttonImportBib_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                htmlBib = ImportHtml();
+                string jsonStringBib = FindJson(htmlBib);
+                richTextBoxOut.Text = jsonStringBib;
+                jsonBib = JObject.Parse(jsonStringBib);
+                labelBibFinded.Text = "Найдено участников: " + jsonBib["persons"].Count();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonExportBibs_Click(object sender, EventArgs e)
+        {
+            ExportNumbers(richTextBoxOut.Text);
         }
     }
 }
